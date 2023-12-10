26f0ea11b6c6b7cbe40ba3016671549d48ec9795
David Cooper <david@dtcooper.com>
2023-12-10 14:49:05 -0500

    test

--- action.py
+++ action.py
@@ -1,5 +1,10 @@
-import pprint
+import sys
 import os
 
+print(f"{sys.version=}")
 
-pprint.pprint(dict(os.environ()))
+print(f"{os.environ['ARG_DOWNLOADED_IMAGE_WORKDIR']=}")
+print(f"{os.environ['ARG_IMAGE_PATH']=}")
+print(f"{os.environ['ARG_FILES']=}")
+print(f"{os.environ['ARG_SCRIPT_PATH']=}")
+print(f"{os.environ['ARG_RUN']=}")
