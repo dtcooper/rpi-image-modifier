5eab28d9a35734c93605af8b8977c9645d67b77c
David Cooper <david@dtcooper.com>
2023-12-11 13:54:53 -0500

    Testing

--- action.sh
+++ action.sh
@@ -76,8 +76,7 @@ SCRIPT_NAME="/tmp/_asdf.sh"  # XXX
 
 if [ "$ARG_RUN" ]; then
     echo "Generating script to run in image container"
-    echo -e '#!/bin/bash\n' | sudo tee "mnt${SCRIPT_NAME}"
-    echo "$ARG_RUN" | sudo tee "mnt${SCRIPT_NAME}"
+    echo -e '#!/bin/bash\n' && echo "$ARG_RUN" | sudo tee "mnt${SCRIPT_NAME}"
 else
     echo "Copying script to run in image container"
     sudo cp -v "${ORIG_DIR}/${ARG_SCRIPT_PATH}" "mnt${SCRIPT_NAME}"
