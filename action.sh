b03efa603fac4e73d4775ff540e7ae81a1d7a4c9
David Cooper <david@dtcooper.com>
2023-12-15 16:06:12 -0500

    Only copy file when cache misses

--- action.sh
+++ action.sh
@@ -33,14 +33,16 @@ cd "${TEMP_DIR}"
 
 if [ -e rpi.img ]; then
     echo 'TESTING: rpi.img already exists'
+    GOT_CACHE_MISS=
 else
-
     if [ "${ARG_CACHE}" -a -e /tmp/rpi-cached.img ]; then
         echo "Using cached image for ${ARG_BASE_IMAGE_URL}"
         mv -v /tmp/rpi-cached.img rpi.img
+        GOT_CACHE_MISS=
     else
         echo "Downloading ${ARG_BASE_IMAGE_URL}..."
         wget -O rpi.img "${ARG_BASE_IMAGE_URL}"
+        GOT_CACHE_MISS=1
     fi
 fi
 
@@ -108,8 +110,8 @@ sudo pishrink.sh rpi.img
 
 echo "Moving image to ${ARG_IMAGE_PATH}"
 
-if [ "${ARG_CACHE}" ]; then
-    echo 'Copying image for cache'
+if [ "${ARG_CACHE}" -a "${GOT_CACHE_MISS}" ]; then
+    echo 'Copying image for cache (got a cache miss)'
     cp -v rpi.img /tmp/rpi-cached.img
 fi
 
